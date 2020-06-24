bc9d7bd476ae14a327d562237dbe1604dd72846e
Piyumal Karunathilaka <piyuk.thilaka@gmail.com>
2020-06-24 19:42:07 +0530

    Structured files

---
+++ src/utils/constants.js
@@ -0,0 +1,17 @@
+const NEWS_API_KEY = '386d9deac797452d8d4fb5c45f6f1cdd';
+const BASE_URL = 'https://newsapi.org/v2';
+
+const REQUEST = 'REQUEST';
+const SUCCESS = '_SUCCESS';
+const FAILURE = '_FAILURE';
+
+const COLORS = {
+    black: 'black',
+    lightgray: '#D8D8D8',
+    darkerGret: '#989898',
+    white: 'white',
+    primaryGreen: '#20BA68',
+    transparentWhite: 'rgba(255, 255, 255, 0.8',
+};
+
+export { NEWS_API_KEY, BASE_URL, SUCCESS, FAILURE, REQUEST, COLORS };
